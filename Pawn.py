0d560bd29b13ed86e3279ad2feb8d9254c2803fd
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-05-21 17:57:26 +0200

    Pawn is moving accordingly to game rules

--- Pawn.py
+++ Pawn.py
@@ -39,8 +39,8 @@ class Pawn(pygame.sprite.Sprite):
     def checkIfPawnIsMovingToTheNearestTile(self, newPawnTile: pygame.sprite.Sprite):
         distanceX = abs(newPawnTile.getTileCenterX() - self.getTileCenterX())
         distanceY = abs(newPawnTile.getTileCenterY() - self.getTileCenterY())
-        print(distanceX)
-        print(distanceY)
+        print(newPawnTile.getTileCenterX())
+        print(newPawnTile.getTileCenterY())
 
         if (distanceX == newPawnTile.image.get_width()) ^ (distanceY == newPawnTile.image.get_height()):
             if (distanceX > newPawnTile.image.get_width()) or (distanceY > newPawnTile.image.get_height()):
@@ -60,26 +60,52 @@ class Pawn(pygame.sprite.Sprite):
         else:
             return False
 
+    def doubleJump(self, newPawnTile: pygame.sprite.Sprite, pawnsSprintTable: pygame.sprite.Group):
+        distanceX = newPawnTile.getTileCenterX() - self.getTileCenterX()
+        distanceY = newPawnTile.getTileCenterY() - self.getTileCenterY()
+
+        if distanceX != 0:
+            if distanceX > 0:
+                pawnInBettwenX = newPawnTile.getTileCenterXForDrawingPawn(self) - newPawnTile.image.get_width()
+                for pawn in pawnsSprintTable:
+                    if pawn.rect.x == pawnInBettwenX and pawn.rect.y == self.rect.y:
+                        return True
+            else:
+                pawnInBettwenX = newPawnTile.getTileCenterXForDrawingPawn(self) + newPawnTile.image.get_width()
+                for pawn in pawnsSprintTable:
+                    if pawn.rect.x == pawnInBettwenX and pawn.rect.y == self.rect.y:
+                        return True
+        else:
+            if distanceY > 0:
+                pawnInBettwenY = newPawnTile.getTileCenterYForDrawingPawn(self) - newPawnTile.image.get_height()
+                for pawn in pawnsSprintTable:
+                    if pawn.rect.y == pawnInBettwenY and pawn.rect.x == self.rect.x:
+                        return True
+            else:
+                pawnInBettwenY = newPawnTile.getTileCenterYForDrawingPawn(self) + newPawnTile.image.get_height()
+                for pawn in pawnsSprintTable:
+                    if pawn.rect.y == pawnInBettwenY and pawn.rect.x == self.rect.x:
+                        return True
 
     def movePawn(self, mx, my, chessTilesSprintTable: pygame.sprite.Group, pawnsSprintTable: pygame.sprite.Group):
         for tileSprite in chessTilesSprintTable:
             if tileSprite.rect.collidepoint((mx, my)):
                 if self.checkIfPawnIsMovingToTheNearestTile(tileSprite):
                     if tileSprite.checkIfContainsPawn(pawnsSprintTable):
-                        self.rect.x = tileSprite.getTileCenterXForDrawingPawn(self)
-                        self.rect.y = tileSprite.getTileCenterYForDrawingPawn(self)
+                        self.setPawnPosition(tileSprite.getTileCenterXForDrawingPawn(self),
+                                             tileSprite.getTileCenterYForDrawingPawn(self))
                         return True
                     else:
                         return False
                 else:
                     if tileSprite.checkIfContainsPawn(pawnsSprintTable):
                         if self.checkIfPawnIsJumpingOver(tileSprite):
-                            print('Double jump possible')
+                            if self.doubleJump(tileSprite, pawnsSprintTable):
+                                self.setPawnPosition(tileSprite.getTileCenterXForDrawingPawn(self),
+                                                     tileSprite.getTileCenterYForDrawingPawn(self))
+                                return True
+
                         else:
-                            print('Double jump not possible')
                             return False
                     else:
-                        print("pawn 2 tiles away")
                         return False
-
-
